82d70e0798ba482276fd4ac33ea9282fd6dd8f78
袁培祥 <yuanpeixiang@carlinx.cn>
2021-11-26 18:18:03 +0800

    init 初始化项目，每日一餐后台搭建

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,10 @@
 <template>
-  <h1>this is login page</h1>
-  <a-button>Hello</a-button>
-  <smile-two-tone spin class="smile" />
+  <div class="background">
+    <a-row class="title">
+      <a-col>你好，欢迎登陆{{}}后台</a-col>
+      <smile-two-tone spin class="smile" />
+    </a-row>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -10,6 +13,21 @@ import {SmileTwoTone} from "@ant-design/icons-vue";
 </script>
 
 <style scoped lang="scss">
+.flex{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 .smile{font-size: 2rem}
-
+.background{
+  width: 100vw;
+  height: 100vh;
+  background: #2c3e50;
+  @extend .flex;
+  flex-direction: column;
+  .title{
+    font-size: 2rem;
+    color: antiquewhite;
+  }
+}
 </style>
